3e0ea776bd53c7514007b79acb62a8554f20ecfd
TheJaredWilcurt <TheJaredWilcurt@users.noreply.github.com>
2023-11-30 19:04:18 -0500

    First two tasks down

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 A timed game where you have to find the virus and press the correct button combination to destroy it. Use with the Omega Virus board.
 
-* [ ] Timer starts when you hit start button and becomes pause button
-* [ ] Timer pauses when you hit pause button
+* [x] Timer starts when you hit start button and becomes pause button
+* [x] Timer pauses when you hit pause button
 * [ ] Add button 30 seconds
 * [ ] Add button 3 minutes
 * [ ] Display sequence of 4 buttons being pressed
+* [ ] When time is low, change color from green to red (color-full color-empty)
--- script.js
+++ script.js
@@ -1,69 +1,40 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function add() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+console.clear();
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+const app = Vue.createApp({
+  data: function () {
+    return {
+      started: false,
+      minutes: 1,
+      remainingSeconds: 0,
+      interval: null
+    };
+  },
+  methods: {
+    initialize: function () {
+      this.remainingSeconds = this.minutes * 60 * 1000;
+    },
+    formatTime: function (seconds) {
+      let date = new Date(0);
+      date.setSeconds(seconds / 1000);
+      let timeString = date.toISOString().substring(11, 19);
+      return timeString;
+    },
+    start: function () {
+      this.started = true;
+      this.interval = setInterval(this.countDown, 1000);
+    },
+    pause: function () {
+      this.started = false;
+      clearInterval(this.interval);
+    },
+    countDown: function () {
+      this.remainingSeconds = this.remainingSeconds - 1000;
+      if (this.remainingSeconds <= 0) {
+        clearInterval(this.interval);
       }
     }
+  },
+  created: function () {
+    this.initialize();
   }
-}
-
-
-//timer
-var minutes = $( '#set-time' ).val();
-
-var target_date = new Date().getTime() + ((minutes * 60 ) * 1000); // set the countdown date
-var time_limit = ((minutes * 60 ) * 1000);
-//set actual timer
-setTimeout(
-  function()
-  {
-    document.getElementById("left").innerHTML = "Out of time!";
-  }, time_limit );
-
-var days, hours, minutes, seconds; // variables for time units
-
-var countdown = document.getElementById("tiles"); // get tag element
-
-setInterval(function () { getCountdown(); }, 1000);
-
-function getCountdown(){
-	// find the amount of "seconds" between now and target
-	var current_date = new Date().getTime();
-	var seconds_left = (target_date - current_date) / 1000;
-
-if ( seconds_left >= 0 ) {
-  console.log(time_limit);
-    if ( (seconds_left * 1000 ) < ( time_limit / 4 ) )  {
-    	$( '#tiles' ).removeClass('color-full');
-    	$( '#tiles' ).addClass('color-empty');
-    }
-
-	days = pad( parseInt(seconds_left / 86400) );
-	seconds_left = seconds_left % 86400;
-
-	hours = pad( parseInt(seconds_left / 3600) );
-	seconds_left = seconds_left % 3600;
-
-	minutes = pad( parseInt(seconds_left / 60) );
-	seconds = pad( parseInt( seconds_left % 60 ) );
-
-	// format countdown string + set tag value
-	countdown.innerHTML = "<span>" + hours + ":</span><span>" + minutes + ":</span><span>" + seconds + "</span>";
-
-}
-}
-
-function pad(n) {
-	return (n < 10 ? '0' : '') + n;
-}
+}).mount('#app');
